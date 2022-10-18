876510187dd3f9fd4d62861fdbc1fe25046d9641
raczylos <sebastian.raczylo@gmail.com>
2022-10-18 06:28:19 +0200

    ppm

--- src/pages/Gk-2.tsx
+++ src/pages/Gk-2.tsx
@@ -1,34 +1,122 @@
 import Canvas from "@/components/Canvas";
 import Line from "@/components/Line";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import rgbHex from "rgb-hex";
 
 type Props = {};
 
 const Gk2 = (props: Props) => {
     const [selectedFile, setSelectedFile] = useState();
-    const changeHandler = (event: any) => {
+    const [ppmAsString, setPpmAsString]: any = useState("");
+    const [ppmFormat, setPpmFormat] = useState("");
+    const [ppmWidth, setPpmWidth] = useState(0);
+    const [ppmHeight, setPpmHeight] = useState(0);
+    const [ppmMaxColorValue, setPpmMaxColorValue] = useState("");
+    const [ppmData, setPpmData] = useState("");
+    const canvasRef = useRef<any>(null!);
+
+    const uploadFile = (event: any) => {
         setSelectedFile(event.target.files[0]);
+
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function () {
+            setPpmAsString(reader.result);
+        };
+        if (file) {
+            // This does not return the text. It just starts reading.
+            // The onload handler is triggered when it is done and the result is available.
+            reader.readAsText(file);
+        }
     };
-    const canvasRef = useRef<HTMLCanvasElement>(null!);
+
+    const splitPpm = () => {
+        // let ppmFormat = ppmAsString.split("\n")[0];
+        // setPpmFormat(ppmAsString.split("\n")[0]);
+        // let ppmSize = ppmAsString.split("\n")[1];
+        // setPpmSize(ppmAsString.split("\n")[1]);
+        // let ppmMaxColorValue = ppmAsString.split("\n")[2];
+        // setPpmMaxColorValue(ppmAsString.split("\n")[2]);
+        // // let xd = ppmAsString.split("\n")[3];
+        // // setPpmData(ppmAsString.split("\n")[3]);
+        // let xd2 =  ppmAsString.split("\n")[2].slice(-1)
+        // // console.log(ppmFormat);
+        // // console.log(ppmSize);
+        // // console.log(ppmMaxColorValue);
+        // console.log(xd2);
+
+        const words = ppmAsString.split(/[\n ]+/);
+        console.log(words)
+        setPpmFormat(words[0]);
+        console.log(words[0]);
+        setPpmWidth(words[1]);
+        console.log(words[1]);
+        setPpmHeight(words[2]);
+        console.log(words[2]);
+        setPpmMaxColorValue(words[3]);
+        console.log(words[3]);
+        setPpmData(words.slice(4));
+        console.log(words.slice(4));
+    };
 
     const handleSubmission = () => {
         const formData = new FormData();
 
         formData.append("File", selectedFile!);
-        console.log(selectedFile);
-        console.log(formData);
+        splitPpm();
     };
+
+    const strokeRectangle = (
+        canvasRef: any,
+        ax: number,
+        ay: number,
+        bx: number,
+        by: number,
+        color: string
+    ) => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context!.strokeStyle = "#000000";
+
+        context?.beginPath();
+        context.rect(ax, ay, bx, by);
+        context?.stroke();
+    };
+
+    // const drawRectangle = () => {
+    //     strokeRectangle(canvasRef, paramAx, paramAy, paramBx, paramBy, color);
+    // };
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.canvas.width = ppmWidth;
+        context.canvas.height = ppmHeight;
+        for (let i = 0; i < ppmWidth; i++) {
+            for (let j = 0; j < ppmHeight; j++) {
+                let colorHex = rgbHex(
+                    parseInt(ppmData[i + j]),
+                    parseInt(ppmData[i + j + 1]),
+                    parseInt(ppmData[i + j + 2])
+                );
+                console.log(colorHex);
+                strokeRectangle(canvasRef, i, j, 1, 1, colorHex);
+            }
+        }
+    }, []);
 
     return (
         <div>
             <p> test </p>
             <canvas ref={canvasRef}></canvas>
-            <input type="file" name="file" onChange={changeHandler} />
+            <input type="file" name="file" onChange={uploadFile} />
             <div>
                 <button onClick={handleSubmission}>Submit</button>
             </div>
+            {/* <img src={ppmAsString} /> */}
         </div>
     );
 };
 
 export default Gk2;
+
