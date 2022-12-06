4f579d508b9d54e79efc899dc4421ca7068c9529
raczylos <sebastian.raczylo@gmail.com>
2022-12-06 15:33:12 +0100

    hit or miss

--- src/components/Morphological.tsx
+++ src/components/Morphological.tsx
@@ -10,6 +10,7 @@ function Morphological({}: Props) {
     const [pixelsDataMatrix, setPixelsDataMatrix] = useState<number[][][][]>(
         []
     );
+    const [mask, setMask] = useState<string>("");
     const [url, setUrl] = useState<string>("");
 
     const onUploadFile = (e: BaseSyntheticEvent) => {
@@ -57,32 +58,33 @@ function Morphological({}: Props) {
                 [] as number[][]
             );
             setPixelsData(pixelsData);
-
-
-
         });
     }, [url]);
 
-    const isNeighborPixelBlackOrWhite = (x: number, y: number, ctx: any, color: string) => {
-        for (let i = x - 1; i <= x + 1; i++) {
+    const isNeighborPixelBlackOrWhite = (
+        x: number,
+        y: number,
+        ctx: any,
+        color: string
+    ) => {
+        for (let i = 0; i < 3 + 1; i++) {
             // console.log("x=" + x)
             // console.log("i=" + i)
-            for (let j = y - 1; j <= y + 1; j++) {
+            for (let j = 0; j < 3; j++) {
                 // console.log("y=" + y)
                 // console.log("j=" + j)
-                let data = ctx.getImageData(i, j, 1, 1).data;
+                let data = ctx.getImageData(x + i - 1, y + j - 1, 1, 1).data;
                 // console.log("data " + data)
-                if(color === "black"){
+                if (color === "black") {
                     if (data[0] === 0 && data[1] === 0 && data[2] === 0) {
                         return true;
                     }
                 }
-                if(color === "white"){
+                if (color === "white") {
                     if (data[0] === 255 && data[1] === 255 && data[2] === 255) {
                         return true;
                     }
                 }
-
             }
         }
         return false;
@@ -99,18 +101,25 @@ function Morphological({}: Props) {
             isNeighborBlackTable[i] = [];
             for (let j = 0; j < bounding.height; j++) {
                 // console.log(isNeighborPixelBlack(i, j, ctx))
-                isNeighborBlackTable[i][j] = isNeighborPixelBlackOrWhite(i, j, ctx, "black");
+                isNeighborBlackTable[i][j] = isNeighborPixelBlackOrWhite(
+                    i,
+                    j,
+                    ctx,
+                    "black"
+                );
             }
         }
 
         for (let i = 0; i < bounding.width; i++) {
             for (let j = 0; j < bounding.height; j++) {
                 let imageData = ctx?.getImageData(i, j, 1, 1);
-                if (isNeighborBlackTable[i][j]) { // jezeli tak to czarny
+                if (isNeighborBlackTable[i][j]) {
+                    // jezeli tak to pixel ustawiamy na kolor czarny
                     imageData!.data[0] = 0;
                     imageData!.data[1] = 0;
                     imageData!.data[2] = 0;
-                } else {  // jak nie to biały
+                } else {
+                    // jak nie to biały
                     imageData!.data[0] = 255;
                     imageData!.data[1] = 255;
                     imageData!.data[2] = 255;
@@ -131,18 +140,25 @@ function Morphological({}: Props) {
             isNeighborWhiteTable[i] = [];
             for (let j = 0; j < bounding.height; j++) {
                 // console.log(isNeighborPixelBlack(i, j, ctx))
-                isNeighborWhiteTable[i][j] = isNeighborPixelBlackOrWhite(i, j, ctx, "white");
+                isNeighborWhiteTable[i][j] = isNeighborPixelBlackOrWhite(
+                    i,
+                    j,
+                    ctx,
+                    "white"
+                );
             }
         }
 
         for (let i = 0; i < bounding.width; i++) {
             for (let j = 0; j < bounding.height; j++) {
                 let imageData = ctx?.getImageData(i, j, 1, 1);
-                if (isNeighborWhiteTable[i][j]) { // jezeli tak to biały
+                if (isNeighborWhiteTable[i][j]) {
+                    // jezeli tak to  pixel ustawiamy na kolor  biały
                     imageData!.data[0] = 255;
                     imageData!.data[1] = 255;
                     imageData!.data[2] = 255;
-                } else {  // jak nie to czarny
+                } else {
+                    // jak nie to czarny
                     imageData!.data[0] = 0;
                     imageData!.data[1] = 0;
                     imageData!.data[2] = 0;
@@ -150,7 +166,6 @@ function Morphological({}: Props) {
                 ctx!.putImageData(imageData!, i, j);
             }
         }
-
     };
 
     const opening = () => {
@@ -158,10 +173,8 @@ function Morphological({}: Props) {
         const bounding = canvas.getBoundingClientRect();
         const ctx = canvas.getContext("2d");
 
-        erosion()
-        dilation()
-
-
+        erosion();
+        dilation();
     };
 
     const closing = () => {
@@ -169,24 +182,95 @@ function Morphological({}: Props) {
         const bounding = canvas.getBoundingClientRect();
         const ctx = canvas.getContext("2d");
 
-        dilation()
-        erosion()
+        dilation();
+        erosion();
+    };
 
+    const changePixel = (x: number, y: number, mask: any, ctx: any) => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const data = ctx.getImageData(x + i - 1, y + j - 1, 1, 1).data;
+                if (mask[i][j] === 1) {
+                    // jezeli biały w matrycy
+                    if (data[0] !== 255 || data[1] !== 255 || data[2] !== 255) {
+                        // jezeli pixel na obazku nie jest biały
+                        return false;
+                    }
+                } else if (mask[i][j] === 0) {
+                    // jezeli czarny w matrycy
+                    if (data[0] !== 0 || data[1] !== 0 || data[2] !== 0) {
+                        // jezeli pixel na obazku nie jest czarny
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     };
 
     const thin = () => {
         const canvas: HTMLCanvasElement = canvasRef.current!;
         const bounding = canvas.getBoundingClientRect();
         const ctx = canvas.getContext("2d");
-        
 
+        let tmp = mask.split(",");
+        let maskMatrix = [];
+        maskMatrix.push([parseInt(tmp[0]), parseInt(tmp[1]), parseInt(tmp[2])]);
+        maskMatrix.push([parseInt(tmp[3]), parseInt(tmp[4]), parseInt(tmp[5])]);
+        maskMatrix.push([parseInt(tmp[6]), parseInt(tmp[7]), parseInt(tmp[8])]);
+        const edges: any = [];
+        for (let i = 0; i < bounding.width; i++) {
+            edges[i] = [];
+            for (let j = 0; j < bounding.height; j++) {
+                edges[i][j] = changePixel(i, j, maskMatrix, ctx);
+            }
+        }
+
+        for (let i = 0; i < bounding.width; i++) {
+            for (let j = 0; j < bounding.height; j++) {
+                if (edges[i][j]) {
+                    const imageData = ctx!.getImageData(i, j, 1, 1);
+
+                    imageData.data[0] = 255;
+                    imageData.data[1] = 255;
+                    imageData.data[2] = 255;
+                    ctx!.putImageData(imageData, i, j);
+                }
+            }
+        }
     };
 
-    const thicken = () => {
+    const embolden = () => {
         const canvas: HTMLCanvasElement = canvasRef.current!;
         const bounding = canvas.getBoundingClientRect();
         const ctx = canvas.getContext("2d");
+        let tmp = mask.split(",");
+        let maskMatrix = [];
+        maskMatrix.push([parseInt(tmp[0]), parseInt(tmp[1]), parseInt(tmp[2])]);
+        maskMatrix.push([parseInt(tmp[3]), parseInt(tmp[4]), parseInt(tmp[5])]);
+        maskMatrix.push([parseInt(tmp[6]), parseInt(tmp[7]), parseInt(tmp[8])]);
 
+        const edges: any = [];
+        for (let i = 0; i < bounding.width; i++) {
+            edges[i] = [];
+            for (let j = 0; j < bounding.height; j++) {
+                edges[i][j] = changePixel(i, j, maskMatrix, ctx);
+            }
+        }
+
+        for (let i = 0; i < bounding.width; i++) {
+            for (let j = 0; j < bounding.height; j++) {
+                if (edges[i][j]) {
+                    const imageData = ctx!.getImageData(i, j, 1, 1);
+
+                    imageData.data[0] = 0;
+                    imageData.data[1] = 0;
+                    imageData.data[2] = 0;
+                    ctx!.putImageData(imageData, i, j);
+                }
+            }
+        }
     };
     return (
         <>
@@ -212,10 +296,40 @@ function Morphological({}: Props) {
                 >
                     erosion
                 </button>
-                <button onClick={() => {opening()}}>opening</button>
-                <button onClick={() => {closing()}}>closing</button>
-                <button onClick={() => {thin()}}>thin</button>
-                <button onClick={() => {thicken()}}>thicken</button>
+                <button
+                    onClick={() => {
+                        opening();
+                    }}
+                >
+                    opening
+                </button>
+                <button
+                    onClick={() => {
+                        closing();
+                    }}
+                >
+                    closing
+                </button>
+                <button
+                    onClick={() => {
+                        thin();
+                    }}
+                >
+                    thin
+                </button>
+                <button
+                    onClick={() => {
+                        embolden();
+                    }}
+                >
+                    embolden
+                </button>
+                <div>
+                    <input
+                        type="text"
+                        onChange={(e) => setMask(e.target.value)}
+                    ></input>
+                </div>
             </div>
             <div>
                 <canvas ref={canvasRef}></canvas>
